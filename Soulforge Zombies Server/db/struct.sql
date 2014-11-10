0678f4b1c12541a3abe915aae2753c46b6e17b26
Fabian Fischer <speisewagen96@gmx.de>
2014-11-10 18:34:18 +0100

    Forgot new struct

--- Soulforge Zombies Server/db/struct.sql
+++ Soulforge Zombies Server/db/struct.sql
@@ -1,18 +1,39 @@
-﻿--
--- Database: `rotmg`
+﻿-- phpMyAdmin SQL Dump
+-- version 4.0.2
+-- http://www.phpmyadmin.net
 --
+-- Host: localhost
+-- Erstellungszeit: 10. Nov 2014 um 18:33
+-- Server Version: 5.5.16
+-- PHP-Version: 5.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `soulforge_zombies`
+--
+CREATE DATABASE IF NOT EXISTS `soulforge_zombies` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `soulforge_zombies`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
+-- Tabellenstruktur für Tabelle `accounts`
 --
 
-CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL auto_increment,
-  `uuid` varchar(128) character set utf8 NOT NULL,
-  `password` varchar(256) character set utf8 NOT NULL,
-  `name` varchar(64) character set utf8 NOT NULL,
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(512) NOT NULL,
+  `uuid` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `name` varchar(64) NOT NULL,
   `admin` tinyint(1) NOT NULL,
   `namechosen` tinyint(1) NOT NULL,
   `verified` tinyint(1) NOT NULL,
@@ -22,17 +43,17 @@ CREATE TABLE `accounts` (
   `maxCharSlot` int(11) NOT NULL,
   `regTime` datetime NOT NULL,
   `guest` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `characters`
+-- Tabellenstruktur für Tabelle `characters`
 --
 
-CREATE TABLE `characters` (
-  `id` int(11) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `accId` int(11) NOT NULL,
   `charId` int(11) NOT NULL,
   `charType` int(11) NOT NULL,
@@ -48,33 +69,33 @@ CREATE TABLE `characters` (
   `tex2` int(11) NOT NULL,
   `pet` int(11) NOT NULL,
   `fameStats` varchar(128) NOT NULL,
-  `createTime` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `deathTime` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deathTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `totalFame` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `classstats`
+-- Tabellenstruktur für Tabelle `classstats`
 --
 
-CREATE TABLE `classstats` (
+CREATE TABLE IF NOT EXISTS `classstats` (
   `accId` int(11) NOT NULL,
   `objType` int(11) NOT NULL,
   `bestLv` int(11) NOT NULL,
   `bestFame` int(11) NOT NULL,
-  PRIMARY KEY  (`accId`,`objType`)
+  PRIMARY KEY (`accId`,`objType`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `death`
+-- Tabellenstruktur für Tabelle `death`
 --
 
-CREATE TABLE `death` (
+CREATE TABLE IF NOT EXISTS `death` (
   `accId` int(11) NOT NULL,
   `chrId` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
@@ -87,50 +108,54 @@ CREATE TABLE `death` (
   `totalFame` int(11) NOT NULL,
   `firstBorn` tinyint(1) NOT NULL,
   `killer` varchar(128) NOT NULL,
-  `time` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`accId`,`chrId`)
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`accId`,`chrId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `news`
+-- Tabellenstruktur für Tabelle `news`
 --
 
-CREATE TABLE `news` (
-  `id` int(11) NOT NULL auto_increment,
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `icon` varchar(16) NOT NULL,
   `title` varchar(128) NOT NULL,
   `text` varchar(128) NOT NULL,
   `link` varchar(256) NOT NULL,
-  `date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stats`
+-- Tabellenstruktur für Tabelle `stats`
 --
 
-CREATE TABLE `stats` (
+CREATE TABLE IF NOT EXISTS `stats` (
   `accId` int(11) NOT NULL,
   `fame` int(11) NOT NULL,
   `totalFame` int(11) NOT NULL,
   `credits` int(11) NOT NULL,
   `totalCredits` int(11) NOT NULL,
-  PRIMARY KEY  (`accId`)
+  PRIMARY KEY (`accId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vaults`
+-- Tabellenstruktur für Tabelle `vaults`
 --
 
-CREATE TABLE `vaults` (
+CREATE TABLE IF NOT EXISTS `vaults` (
   `accId` int(11) NOT NULL,
-  `chestId` int(11) NOT NULL auto_increment,
+  `chestId` int(11) NOT NULL AUTO_INCREMENT,
   `items` varchar(128) NOT NULL,
-  PRIMARY KEY  (`accId`,`chestId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`accId`,`chestId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
